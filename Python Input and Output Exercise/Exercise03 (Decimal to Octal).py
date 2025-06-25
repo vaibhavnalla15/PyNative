eba615dcb0a95228b046834e46f638b55113d145
vaibhav <vaibhavnalla15@gmail.com>
2025-06-25 19:49:17 +0530

    Added Exercise 3

---
+++ Python Input and Output Exercise/Exercise03 (Decimal to Octal).py
@@ -0,0 +1,11 @@
+""" Display Decimal Number to Octal using print() function """
+# Hints:-
+# Use the %o formatting code within the print() function to format a decimal number as octal.
+
+num = 8
+print('%o' % num)
+
+# or
+num = 8
+print(f"{num:o}")         # Output: 10
+print("{:o}".format(num)) # Output: 10
