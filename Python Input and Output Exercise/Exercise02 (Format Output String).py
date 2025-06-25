3da6a16d2d5f11a4ebb6abede61021c0afd46c98
vaibhav <vaibhavnalla15@gmail.com>
2025-06-25 14:04:57 +0530

    Added Exercise 2

---
+++ Python Input and Output Exercise/Exercise02 (Format Output String).py
@@ -0,0 +1,12 @@
+""" Write a program to display four string “My, “Name“, “Is“, “James” as “My**Name**Is**James“. """
+# Use the print() function to format the given words in the specified format. Display the ** separator between each string.
+# For example: print('your code') should display My**Name**Is**James
+# Hints:-
+# Use the sep parameter of the print() function to define the separator symbol between each word.
+
+str1 = "My"
+str2 = "Name"
+str3 = "Is"
+str4 = "James"
+
+print(str1, str2, str3, str4, sep="**")
