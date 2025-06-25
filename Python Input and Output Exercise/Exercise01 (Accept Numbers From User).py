0395f5d14630aafdc27354474e8148550b6c059d
vaibhav <vaibhavnalla15@gmail.com>
2025-06-25 13:37:20 +0530

    Added Exercise 1

---
+++ Python Input and Output Exercise/Exercise01 (Accept Numbers From User).py
@@ -0,0 +1,6 @@
+""" Write a program to accept two integer numbers from the user and calculate their product. """
+num1 = int(input("Enter the 1st Number:- "))
+num2 = int(input("Enter the 2nd Number:- "))
+
+print(f"The product of {num1} & {num2} is: {num1 * num2}")
+
