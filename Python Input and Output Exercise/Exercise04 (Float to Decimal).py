75473da92d80e86d7725dc72a686902704e8ec36
vaibhav <vaibhavnalla15@gmail.com>
2025-06-25 19:57:55 +0530

    Added Exercise 4

---
+++ Python Input and Output Exercise/Exercise04 (Float to Decimal).py
@@ -0,0 +1,6 @@
+""" Display Float Number with 2 Decimal Places """
+# Example =  num = 458.541315 to 458.54
+# Hints :- Use the %.2f formatting code within the print() function to format a float number to two decimal places
+
+num = 458.541315
+print("%.2f" % num)
