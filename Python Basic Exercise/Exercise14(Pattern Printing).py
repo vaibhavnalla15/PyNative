661a013c6f48fb0cc55b30d74301919c3bdbbd77
vaibhav <vaibhavnalla15@gmail.com>
2025-06-30 12:42:40 +0530

    Updated Exercise 14

--- Python Basic Exercise/Exercise14(Pattern Printing).py
+++ Python Basic Exercise/Exercise14(Pattern Printing).py
@@ -102,3 +102,10 @@ for i in range(1, rows + 1):
             print(j, end=" ")
     print()
 
+print("Print the reverse number pattern")
+n = 5
+k = 5
+for i in range(0,n+1):
+    for j in range(k-i,0,-1):
+        print(j,end=' ')
+    print()
