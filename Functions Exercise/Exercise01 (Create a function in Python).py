ed112b3369e169d9e488efb4de481a6d62b5b818
vaibhav <vaibhavnalla15@gmail.com>
2025-07-02 13:36:39 +0530

    Added Exercise 1

---
+++ Functions Exercise/Exercise01 (Create a function in Python).py
@@ -0,0 +1,7 @@
+""" Create a function in Python """
+# Write a program to create a function that takes two arguments, name and age, and prints their values.
+
+def first_test(name, age):
+    print("The name is",name, "and the age is",age)
+
+first_test(name="Vaibhav", age=23)
