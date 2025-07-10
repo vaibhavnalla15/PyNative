22df486295c90200b01da9b67f5414e2e0ab7af0
vaibhav <vaibhavnalla15@gmail.com>
2025-07-10 12:25:30 +0530

    Added Exercise 9

---
+++ Functions Exercise/Exercise09 (Find the largest item from list).py
@@ -0,0 +1,10 @@
+""" Find the largest item from list """
+# Hints:- Use the built-in function max() to get the largest number from a list
+
+# Given :-
+x = [4, 6, 8, 24, 12, 2]  # Answer should be 24
+
+def largest_num():
+    print(max(x))
+
+largest_num()
