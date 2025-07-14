356e8a0d555e0cb19e8d0a3e07c341d00012c6fd
vaibhav <vaibhavnalla15@gmail.com>
2025-07-14 13:50:12 +0530

    Added Exercise 9

--- String Exercise/Exercise09 (Calculate the sum and average of the digits present in a string).py
+++ String Exercise/Exercise09 (Calculate the sum and average of the digits present in a string).py
@@ -18,3 +18,15 @@ for i in str1:
 avg = total / cnt
 print("Sum is:-", total, "\nAverage is:-", avg)
 
+# Using Regular Expression:-
+import re
+input_str = "PYnative29@#8496"
+digit_list = [int(num) for num in re.findall(r'\d', input_str)]
+print('Digits:', digit_list)
+
+# use the built-in function sum
+total = sum(digit_list)
+
+# average = sum / count of digits
+avg = total / len(digit_list)
+print("Sum is:", total, "Average is ", avg)
