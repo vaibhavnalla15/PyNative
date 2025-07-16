daf24bf63416975fb6deda66c8850d6842d90287
vaibhav <vaibhavnalla15@gmail.com>
2025-07-16 22:18:39 +0530

    Added Exercise 4

---
+++ Python List Exercise/Exercise04 (Reverse a list).py
@@ -0,0 +1,11 @@
+""" Reverse a list """
+list1 = [100, 200, 300, 400, 500]
+
+# Using list slicing
+reversed_list = list1[::-1]
+print("Reversed List is:-", reversed_list)
+
+# Using built-in function reverse()
+list1 = [100, 200, 300, 400, 500]
+list1.reverse()
+print("Reversed List is:-", list1)
