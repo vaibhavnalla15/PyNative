f2a366d619e75ed883758e769e4f7b5d8ae4feb1
vaibhav <vaibhavnalla15@gmail.com>
2025-07-17 11:36:10 +0530

    Added Exercise 7

---
+++ Python List Exercise/Exercise07 (Count Occurrences).py
@@ -0,0 +1,7 @@
+""" Count Occurrences """
+# Count and print how many times 'Football' appears in list.
+# Hints:- Python lists have a count() method that allows you to count the occurrences of a specific element within the list.
+
+sports = ['Cricket', 'Football', 'Hockey', 'Football', 'Tennis']
+print(sports.count("Football"))
+
