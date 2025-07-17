30c60c5e6fd222f09a8c85f00f31303f6b5f4ca7
vaibhav <vaibhavnalla15@gmail.com>
2025-07-17 20:31:56 +0530

    Added Exercise 4

---
+++ Python Dictionary Exercise/Exercise04 (Clear Dictionary).py
@@ -0,0 +1,9 @@
+""" Clear Dictionary """
+# Clear all key-value pairs from a given dictionary and print it.
+# Hints:- Use dictionary method clear() to remove all items.
+
+my_dict = {'name': 'Alice', 'age': 35, 'city': 'New York'}
+print(f"dictionary: {my_dict}")
+
+my_dict.clear()
+print(f"dictionary after removing all items: {my_dict}")
