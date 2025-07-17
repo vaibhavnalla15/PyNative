93a70bf19ef57a092374908c1a65803f7cc75ee8
vaibhav <vaibhavnalla15@gmail.com>
2025-07-17 14:07:26 +0530

    Added Exercise 19

--- Python List Exercise/Exercise19 (Iterate both lists simultaneously).py
+++ Python List Exercise/Exercise19 (Iterate both lists simultaneously).py
@@ -2,7 +2,10 @@
 # Given a two Python list. Write a program to iterate both lists simultaneously and display items from list1 in original order
 # and items from list2 in reverse order.
 # Hints:-
-# Use the zip() function. This function takes two or more iterables (like list, dict, string), aggregates them in a tuple, and returns it.
+# 1. Use the zip() function. This function takes two or more iterables (like list, dict, string), aggregates them in a tuple, and returns it.
+# 2. The zip() function can take two or more lists, aggregate them in a tuple, and returns it.
+# 3. Pass the first argument as a list1 and seconds argument as a list2[::-1] (reverse list using list slicing)
+# 4. Iterate the result using a for loop
 
 list1 = [10, 20, 30, 40]
 list2 = [100, 200, 300, 400]
@@ -16,5 +19,3 @@ print()
 # In reverse order:-
 for x, y in zip(list1, list2[::-1]):
     print(x, y)
-
-
