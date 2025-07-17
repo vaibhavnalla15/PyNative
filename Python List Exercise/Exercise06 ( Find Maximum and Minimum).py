f2d2a3969385d15fb5d0655d27d69801a5e6310f
vaibhav <vaibhavnalla15@gmail.com>
2025-07-17 11:33:44 +0530

    Added Exercise 6

---
+++ Python List Exercise/Exercise06 ( Find Maximum and Minimum).py
@@ -0,0 +1,8 @@
+"""  Find Maximum and Minimum """
+# Find and print the largest and smallest number in a list [8, 2, 15, 1, 9].
+# Hints:- Python provides built-in functions max() and min() for finding the maximum and minimum values in a list.
+
+data = [8, 2, 15, 1, 9]
+
+print("Largest number:-", max(data))
+print("Smallest number:-", min(data))
