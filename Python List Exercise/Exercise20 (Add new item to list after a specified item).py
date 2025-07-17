1dc27212dbd26eb87675c81a04500122de604860
vaibhav <vaibhavnalla15@gmail.com>
2025-07-17 14:27:45 +0530

    Added Exercise 20

--- Python List Exercise/Exercise20 (Add new item to list after a specified item).py
+++ Python List Exercise/Exercise20 (Add new item to list after a specified item).py
@@ -4,5 +4,13 @@
 # The given list is a nested list. Use indexing to locate the specified item, then use the append() method to add a new item after it.
 
 list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+
+# understand indexing
+# list1[0] = 10
+# list1[1] = 20
+# list1[2] = [300, 400, [5000, 6000], 500]
+# list1[2][2] = [5000, 6000]
+# list1[2][2][1] = 6000
+
 list1[2][2].append(7000)
 print(list1)
