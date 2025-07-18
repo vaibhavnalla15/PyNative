a51dd04c49973a47a5066e162aaf9ec95f5de6f0
vaibhav <vaibhavnalla15@gmail.com>
2025-07-18 16:36:16 +0530

    Added Exercise 11

--- Python Dictionary Exercise/Exercise11 (Create a dictionary by extracting the keys from a given dictionary).py
+++ Python Dictionary Exercise/Exercise11 (Create a dictionary by extracting the keys from a given dictionary).py
@@ -4,6 +4,7 @@
 # 1. Iterate the mentioned keys using a loop
 # 2. Next, check if the current key is present in the dictionary, if it is present, add it to the new dictionary
 
+# Solution 1: Dictionary Comprehension
 sample_dict = {
     "name": "Kelly",
     "age": 25,
@@ -17,3 +18,19 @@ keys = ["name", "salary"]
 new_dict = {k: sample_dict[k] for k in keys}
 print(new_dict)
 
+# Solution 2: Using the update() method and loop
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# keys to extract
+keys = ["name", "salary"]
+
+# new dict
+result = dict()
+
+for k in keys:
+    result.update({k: sample_dict[k]})
+print(result)
