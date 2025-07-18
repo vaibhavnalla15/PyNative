e9d8ce02dc946ef53fd2560526ff8d98c057628c
vaibhav <vaibhavnalla15@gmail.com>
2025-07-18 20:11:48 +0530

    Added Exercise 15

---
+++ Python Dictionary Exercise/Exercise15 (Get the key of a minimum value).py
@@ -0,0 +1,12 @@
+""" Get the key of a minimum value """
+# Write a code to print the key of a minimum value from the following dictionary.
+# Hints:- Use the built-in function min()
+
+sample_dict = {
+  'Physics': 82,
+  'Math': 65,
+  'history': 75
+}
+
+print(min(sample_dict))
+
