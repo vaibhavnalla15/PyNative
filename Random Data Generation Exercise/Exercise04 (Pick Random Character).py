f54b6aedd968a374f16e319a3a72477ff02df051
vaibhav <vaibhavnalla15@gmail.com>
2025-07-21 12:35:17 +0530

    Added Exercise 4

---
+++ Random Data Generation Exercise/Exercise04 (Pick Random Character).py
@@ -0,0 +1,6 @@
+""" Pick Random Character """
+# Write a code to select a random character from a given string name = 'pynative'.
+import random
+
+string = 'pynative'
+print("Random Character is:-", random.choice(string))
