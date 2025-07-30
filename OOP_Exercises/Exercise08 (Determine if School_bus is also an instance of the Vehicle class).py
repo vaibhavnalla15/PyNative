bda5b5bb1197fc63c126937a571f09781761bfb0
vaibhav <vaibhavnalla15@gmail.com>
2025-07-30 19:21:32 +0530

    Added Exercise 8

---
+++ OOP_Exercises/Exercise08 (Determine if School_bus is also an instance of the Vehicle class).py
@@ -0,0 +1,19 @@
+""" Determine if School_bus is also an instance of the Vehicle class """ 
+# Hints:- Use isinstance() function
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+class Bike:                                     # This class does not belongs to any object we use.
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+print(isinstance(School_bus, Vehicle))          # This class Vehicle give True, also we can use Bus class also.
+
+print(isinstance(School_bus, Bike))             # This class Bike give False, because School bus object does not belong to Bike class.
